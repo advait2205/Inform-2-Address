13e7a59cc915799e376a7bc690d9cfa1c6f6c877
Advait Acharya <advaitacharya22@gmail.com>
2022-06-25 12:23:42 +0530

    Infinte Scroll Improved and filter dropdown added

--- users/static/styles/complains/script.js
+++ users/static/styles/complains/script.js
@@ -1,44 +1,80 @@
-console.log(complains);
+ console.log(complains);
+  
+  const container = document.getElementById('container');
+  const loading = document.querySelector('.loading');
+  let post_offset = 0;
+  
+//   getPost();
+//   getPost();
+//   getPost();
 
-const container = document.querySelector('.container');
-
-let limit = 4;
-let pageCount = 1;
-let postCount = 0;
-
-const getPost = () => {
-
-    let i = 0;
-    for(i = 0; i < limit; i++){
-        const htmlData = `
-        <div class="posts">
-        <p class="post-id">${postCount}</p>    
-        <h2 class="title">${complains[postCount].city}</h2>
-        <p class="post-info">${complains[postCount].text}</p>
-        </div>
-        `;
-        postCount++;
-
-        container.insertAdjacentHTML('beforeend', htmlData);
-    }
-};
-
-
-getPost();
-
-const showData = () => {
-    setTimeout(() => {
-        pageCount++;
+    for (let index = 0; index < 5; index++) {
         getPost();
-    }, 300)
+    }
+  
+  window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  
+    if(clientHeight + scrollTop >= scrollHeight - 5) {
+      // show the loading animation
+      showLoading();
+    }
+  });
+  
+  function showLoading() {
+    if(post_offset < complains.length){
+      loading.classList.add('show');
+  
+      // load more data
+      setTimeout(getPost, 300)
+    }
+    else{
+      // end has been reached, no more posts available
+    }
+  }
+  
+  async function getPost() {
+    console.log(post_offset)
+    if(post_offset < complains.length){
+      addDataToDOM(complains[post_offset]);
+      post_offset++;
+    }
+  }
+  // https://i.ibb.co/SPjZ97Y/imgonline-com-ua-Replace-Color-s-Z9-Bt-Ug911i-A.jpg  -- green image
+  function addDataToDOM(data) {
+    const postElement = document.createElement('div');
+    postElement.classList.add('blog-post');
+    postElement.innerHTML = `
+      <h2 class="title">${data.city}</h2>
+      <img class="post_image" src="${data.image_url}">
+      <p class="text">${data.text}</p>
+      <img class="post_upvote" src="https://i.ibb.co/z84z4QD/b9zcqp6w31w51-removebg-preview.jpg" id="post_upv" width="8%" height="8%" onclick="changeImage()">
+    `;
+    container.appendChild(postElement);
+  
+    loading.classList.remove('show');
+  }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.addEventListener('scroll', () => {
-    // console.log('Hello');
-    const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
-    // console.log(scrollHeight, scrollTop, clientHeight);
-    if(scrollTop + clientHeight > scrollHeight - (1.2 * pageCount)){
-        console.log('I am at bottom');
-        showData();
+function btn_click1(){
+	var x=document.getElementById("fuckoff")
+    x.innerHTML="aaah"
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
     }
-});
+  }
+
+//   function changeImage(){
+//     if(document.getElementById("post_upv").src == "https://i.ibb.co/z84z4QD/b9zcqp6w31w51-removebg-preview.jpg"){
+//         document.getElementById("post_upv").src= "https://i.ibb.co/SPjZ97Y/imgonline-com-ua-Replace-Color-s-Z9-Bt-Ug911i-A.jpg"
+//     }
+// }
