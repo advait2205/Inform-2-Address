972e9fb2d3c7c9da818305e39c9e903d83b1e1fd
Ronak-Jethava <ronakjethava1511@gmail.com>
2022-06-30 02:21:59 +0530

    admin page data retrieved

--- admin/views.py
+++ admin/views.py
@@ -17,7 +17,18 @@ def get_statistics(request):
     authorities = [dict(zip(colnames, authority)) for authority in authorities]
     authorities = json.dumps(authorities, indent=4, sort_keys=True, default=str)
     
+    c.execute(f'''
+        SELECT *
+        FROM my_db.complains
+    ''')
+
+    colnames = [desc[0] for desc in c.description]
+
+    complains = c.fetchall()
+    complains = [dict(zip(colnames, complain)) for complain in complains]
+    complains = json.dumps(complains, indent=4, sort_keys=True, default=str)
+
     conn.close()
     
-    return render(request, "admin_page.html", {"authorities": authorities})
+    return render(request, "admin_page.html", {"authorities": authorities, "complains": complains})
 
