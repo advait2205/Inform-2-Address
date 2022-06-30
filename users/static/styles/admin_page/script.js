631d8ff159774b73618c688e8bef832153748de7
Advait Acharya <advaitacharya22@gmail.com>
2022-06-30 14:54:17 +0530

    Expected Resolve date field added

--- users/static/styles/admin_page/script.js
+++ users/static/styles/admin_page/script.js
@@ -59,6 +59,7 @@ function addDataToDOM(data) {
     <img class="post_image" src="${data.image_url}">
     <p class="text">${data.text}</p>
     <p class="date">${"Posted on: " + data.start_time.substring(0,10)}</p>
+    <p class="expected_resolve_date">${"Expected Resolve Date: " + data.end_time.substring(0,10)}</p>
     <img class="post_upvote" src="https://i.ibb.co/z84z4QD/b9zcqp6w31w51-removebg-preview.jpg" id="post_upv" width="8%" height="8%" >
   `;
   container.appendChild(postElement);
@@ -87,101 +88,3 @@ window.onclick = function(e) {
 //         document.getElementById("post_upv").src= "https://i.ibb.co/SPjZ97Y/imgonline-com-ua-Replace-Color-s-Z9-Bt-Ug911i-A.jpg"
 //     }
 // }
-
-// location modal script
-
-var modal = document.getElementById("locationModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btn_onClick_location");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-var scnt=0, regcnt=0, citycnt=0, startcnt=0, owncompcnt=0; //onclick counters
-
-function btn_onClick_State(){
-++scnt;
-regcnt=0;
-citycnt=0;
-startcnt=0;
-owncompcnt=0;
-modal.style.display = "block";
-document.getElementById("modal_header").innerHTML="Enter the State name which you want to filter";
-}
-
-function btn_onClick_Region(){
-++regcnt;
-scnt=0;
-citycnt=0;
-startcnt=0;
-owncompcnt=0;
-modal.style.display = "block";
-document.getElementById("modal_header").innerHTML="Enter the Region name which you want to filter"
-}
-
-function btn_onClick_City(){
-++citycnt;
-scnt=0;
-regcnt=0;
-startcnt=0;
-owncompcnt=0;
-modal.style.display = "block";
-document.getElementById("modal_header").innerHTML="Enter the City name which you want to filter"
-}
-
-function btn_onClick_StartTime(){
-++startcnt;
-scnt=0;
-regcnt=0;
-citycnt=0;
-owncompcnt=0;
-modal.style.display = "block";
-document.getElementById("modal_header").innerHTML="Enter the Start Time name which you want to filter"
-}
-
-function btn_onClick_owncomp(){
-++owncompcnt;
-scnt=0;
-regcnt=0;
-citycnt=0;
-startcnt=0;
-modal.style.display="block";
-document.getElementById("modal_header").innerHTML="Enter your Mobile Number";
-}
-function submit_onclick(){
-  var filter_val=document.getElementById("loc").value;
-  if(filter_val === ""){
-      alert("Field cannot be kept empty!")
-  }else{
-    if(scnt > 0){
-      alert('state');
-    }else if(regcnt > 0){
-      alert('region');
-    }else if(citycnt  > 0){
-      alert('city');
-    }else if(startcnt > 0){
-      alert('start');
-    }else if(owncompcnt > 0){
-      alert(filter_val);
-    }
-    modal.style.display="none";
-  }
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-  modal.style.display = "none";
-}
-}
-
-function fab_onClick(){
-location.href="add_complain"
-}
